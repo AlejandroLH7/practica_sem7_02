80f9e198df1ea91c678de1bb551c5adef394a864
audio <audio@LAPTOP-A2DKNN9M>
2023-10-04 22:34:51 -0500

    Agregar archivos de proyecto.

---
+++ practica_sem7_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_sem7_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    cont++;
+                    Console.WriteLine("Divisor de  " + "Alejandro Ledesma" + ": " + i);
+                    Console.ReadKey();
+                }
+            }
+            
+        }
+    }
+}
